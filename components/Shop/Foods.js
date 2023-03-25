6fbb1f3884962c60e9e9e2520530f103cee7ef38
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-03-25 23:28:52 +0600

    implemented filter by category name

--- components/Shop/Foods.js
+++ components/Shop/Foods.js
@@ -3,12 +3,11 @@ import React, { useContext } from 'react'
 import Food from './Food'
 
 
-const Foods = () => {
-    const { foods } = useContext(Context)
+const Foods = ({ foods }) => {
 
     return (
         <section className='bg-slate-300 rounded-md mx-6 lg:mx-24 p-3 lg:p-8 grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-8 mb-14 mt-6'>
-            {foods.map(food => <Food
+            {foods?.map(food => <Food
                 key={food.id}
                 food={food}
             ></Food>)}
