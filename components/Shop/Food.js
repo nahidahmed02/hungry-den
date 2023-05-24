f94210daee4beb71c6d681e61c8a92f19e1ac6a5
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-05-25 00:31:48 +0600

    change in card UI

--- components/Shop/Food.js
+++ components/Shop/Food.js
@@ -56,38 +56,43 @@ const Food = ({ food }) => {
 
     return (
         <section>
-            <div className="card bg-slate-200 shadow-xl py-3">
+            <div className="card bg-slate-200 shadow-xl">
 
-                <figure className="px-4 pt-2 lg:px-6 lg:pt-6 ">
-                    <Image src={photo} alt="food" width={170} height={95} className="rounded-xl mb-3 hover:w-96" />
+                <figure>
+                    <Image src={photo} alt="food" width={192} height={95} className="rounded-t-md mt-7 mb-4 hover:w-96" />
                 </figure>
 
                 <div className="text-sm mx-4 lg:mx-8">
+
                     <h2 className="mb-0.5">Category: {category}</h2>
                     <h2 className="mb-0.5">Name: {name}</h2>
                     <h2 className="mb-0.5">Price: ${price} + vat({vat}%)</h2>
+
                     <h2 className="mb-0.5">Quantity:
                         <span onClick={decrement} className='cursor-pointer font-bold bg-slate-300 px-1 lg:px-1.5 lg:pb-1 ml-2 lg:ml-3 mr-1'>-</span>
                         <span className='px-2'>{(isAddedToCart) ? orderedFood.quantity : quantity}</span>
                         <span onClick={increment} className='cursor-pointer font-bold bg-slate-300 px-1 lg:pb-1 ml-1'>+</span>
                     </h2>
+
                     <h2 className="mb-0.5">Total: ${total}</h2>
 
-                    <div className="card-actions mt-2">
-                        {(!isAddedToCart) && <button className="rounded-lg px-2 text-white font-bold btn-xs bg-blue-500 mx-auto"
-                            onClick={handleAddToCart}
-                        >Add to Cart</button>}
+                </div>
 
-                        {(isAddedToCart) && <>
-                            <button className="rounded-lg px-2 text-white font-bold btn-xs bg-green-600 mx-auto"
-                                onClick={() => router.push('/cart')}
-                            >Go to Cart</button>
-                            <button className="rounded-lg px-2 text-white font-bold btn-xs bg-red-700 mx-auto"
-                                onClick={handleRemoveFromCart}
-                            >Remove From Cart</button>
-                        </>}
+                <div className="card-actions my-4 mx-5">
 
-                    </div>
+                    {(!isAddedToCart) && <button className="rounded-lg px-2 text-white font-bold btn-xs bg-blue-500 mx-auto"
+                        onClick={handleAddToCart}
+                    >Add to Cart</button>}
+
+                    {(isAddedToCart) && <>
+                        <button className="rounded-lg px-2 text-white font-bold btn-xs bg-green-600 mx-auto"
+                            onClick={() => router.push('/cart')}
+                        >Go to Cart</button>
+                        <button className="rounded-lg px-2 text-white font-bold btn-xs bg-red-700 mx-auto"
+                            onClick={handleRemoveFromCart}
+                        >Remove From Cart</button>
+                    </>}
+
                 </div>
             </div>
         </section>
