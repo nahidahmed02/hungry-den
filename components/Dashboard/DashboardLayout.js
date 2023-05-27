586209d2af24f4b65f6780d8e8dadea06f3a92e3
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-05-27 23:49:01 +0600

    dashboard sidebar opt added

--- components/Dashboard/DashboardLayout.js
+++ components/Dashboard/DashboardLayout.js
@@ -3,21 +3,26 @@ import React from 'react'
 
 const DashboardLayout = ({ children }) => {
     return (
-        <section className='mt-24 mb-20'>
+        <section className='mt-24 mb-8 border border-black'>
 
             <div className="drawer drawer-mobile">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content">
                     <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
-                    <h2 className='ml-6'>Dashboard</h2>
+                    <h2 className='ml-6 text-3xl font-serif font-bold text-orange-500'>Dashboard</h2>
 
                     {children}
                 </div>
                 <div className="drawer-side mb-auto mt-14 lg:ml-3">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                    <ul className="menu p-4 overflow-y-auto w-52 bg-orange-400 text-base-content">
-                        <li className='text-white font-bold'><Link href='/dashboard/addItem'>Add Item</Link></li>
-                        <li className='text-white font-bold'><Link href='/dashboard/deleteItem'>Delete Item</Link></li>
+                    <ul className="menu p-4 overflow-y-auto w-52 text-base-content">
+                        <li className='text-white font-bold bg-orange-400 rounded-md mb-1'><Link href='/dashboard/profile'>Profile</Link></li>
+                        <li className='text-white font-bold bg-orange-400 rounded-md mb-1'><Link href='/dashboard/orders'>Orders</Link></li>
+                        <li className='text-white font-bold bg-orange-400 rounded-md mb-1'><Link href='/dashboard/addItem'>Add Item</Link></li>
+                        <li className='text-white font-bold bg-orange-400 rounded-md mb-1'><Link href='/dashboard/deleteItem'>Delete Item</Link></li>
+                        <li className='text-white font-bold bg-orange-400 rounded-md mb-1'><Link href='/dashboard/manageUsers'>Manage Users</Link></li>
+                        <li className='text-white font-bold bg-orange-400 rounded-md mb-1'><Link href='/dashboard/myOrders'>My Orders</Link></li>
+                        <li className='text-white font-bold bg-orange-400 rounded-md mb-1'><Link href='/dashboard/addReview'>Add Review</Link></li>
                     </ul>
 
                 </div>
