2ab72291bf32f15d4fba01272d8029645473005e
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-05-28 12:50:31 +0600

    dashboard drawer button style changed

--- components/Dashboard/DashboardLayout.js
+++ components/Dashboard/DashboardLayout.js
@@ -8,7 +8,9 @@ const DashboardLayout = ({ children }) => {
             <div className="drawer drawer-mobile">
                 <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
                 <div className="drawer-content">
-                    <label htmlFor="my-drawer-2" className="btn btn-sm drawer-button lg:hidden">Open drawer</label>
+                    <label htmlFor="my-drawer-2" tabIndex={1} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
                     <h2 className='ml-6 text-3xl font-serif font-bold text-orange-500'>Dashboard</h2>
 
                     {children}
