e888987278900a680e6a010d29cd21048a791efe
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-06-13 22:08:39 +0600

    grids for tab

--- src/components/Shop/Foods.js
+++ src/components/Shop/Foods.js
@@ -8,7 +8,7 @@ const Foods = () => {
     let { currentFilteredFoods, searchQuery, searchResult } = useContext(Context);
 
     return (
-        <section className='rounded-md mx-4 lg:mx-16 p-3 lg:p-8 grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-8 mb-4'>
+        <section className='rounded-md mx-4 lg:mx-16 p-3 md:p-5 lg:p-8 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3 md:gap-5 lg:gap-8 mb-4'>
             {
                 searchQuery && searchResult?.map(food => <Food
                     key={food.id}
