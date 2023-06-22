a62e397206646404ee6fd02539e8c08512e3ab4a
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-06-22 16:55:17 +0600

    initial UI for user profile

--- src/components/Dashboard/DashboardLayout.js
+++ src/components/Dashboard/DashboardLayout.js
@@ -18,14 +18,13 @@ const DashboardLayout = ({ children }) => {
                     <label htmlFor="my-drawer-2" tabIndex={1} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-                    <h2 className='ml-6 text-3xl font-serif font-bold text-orange-500'>Dashboard</h2>
+
 
                     {children}
                 </div>
 
                 <div className="drawer-side mb-auto mt-8 sticky">
                     <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-
                     <ul className="menu p-4 overflow-y-auto w-52 text-base-content">
                         <li className='text-white font-bold bg-orange-400 rounded-md mb-1'>
                             <Link href='/dashboard'>
--- src/components/Dashboard/Profile.js
+++ src/components/Dashboard/Profile.js
@@ -1,8 +1,29 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Profile = () => {
     return (
-        <div>Profile</div>
+        <div className=''>
+            <h2 className='mt-6 text-2xl font-serif font-bold text-orange-500 text-center'>Profile</h2>
+            <div className="mx-auto card w-96 bg-base-100 shadow-2xl border mt-5">
+                <figure className="px-10 pt-10">
+                    <Image
+                        fill='auto'
+                        alt='user'
+                        src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                        className="rounded-xl" />
+                </figure>
+                <div className="card-body">
+                    <p>Name:</p>
+                    <p>E-Mail:</p>
+                    <p>Phone:</p>
+                    <p>Address:</p>
+                    <div className="card-actions mt-3">
+                        <button className="btn btn-xs border-none bg-orange-500">Edit</button>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
