5711d16b69751135b90545c938ef52ff5c1e7d6e
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-06-25 00:27:07 +0600

    color updated in signup page

--- src/components/Register/SignUp.js
+++ src/components/Register/SignUp.js
@@ -94,7 +94,7 @@ const SignUp = () => {
 
                 <button
                     type="submit"
-                    className="bg-orange-400 text-white font-bold w-full max-w-xs mx-auto py-2 rounded-md">
+                    className="bg-orange-500 text-white font-bold w-full max-w-xs mx-auto py-2 rounded-md">
 
                     Sign Up
                 </button>
