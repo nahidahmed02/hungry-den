55141cfe68a8508cd39cef775f4118fab29ba56d
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-06-26 23:52:36 +0600

    margin given to childrens of dashboard to align then center

--- src/components/Dashboard/DashboardLayout.js
+++ src/components/Dashboard/DashboardLayout.js
@@ -18,8 +18,9 @@ const DashboardLayout = ({ children }) => {
                     <label htmlFor="my-drawer-2" tabIndex={1} className="btn btn-ghost lg:hidden">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                     </label>
-
-                    {children}
+                    <div className='lg:ml-48'>
+                        {children}
+                    </div>
                 </div>
 
                 <div className="drawer-side mb-auto mt-8 lg:fixed">
