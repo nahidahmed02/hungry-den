d11dadee94559b20f5615b78924061e33fcbd8a7
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-07-09 16:16:49 +0600

    react query added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,14 +2,20 @@ import '@/styles/globals.css'
 import { ContextProvider } from '../context/Context'
 import AuthProvider from '../context/AuthProvider'
 import { Toaster } from 'react-hot-toast'
+import { QueryClient, QueryClientProvider } from 'react-query'
+
+const queryClient = new QueryClient()
 
 export default function App({ Component, pageProps }) {
   return (
-    <AuthProvider>
-      <ContextProvider>
-        <Component {...pageProps} />
-        <Toaster></Toaster>
-      </ContextProvider>
-    </AuthProvider>
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        <ContextProvider>
+          <Component {...pageProps} />
+          <Toaster></Toaster>
+        </ContextProvider>
+      </AuthProvider>
+    </QueryClientProvider>
+
   )
 }
