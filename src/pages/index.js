95fcae993f49f398b29076827032f995b3b1d7eb
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-07-18 21:28:34 +0600

    cart icon added in home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import Layout from '../components/Layout/Layout'
 import Shop from '../components/Shop/Shop'
+import { BsCart4 } from 'react-icons/bs'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +20,9 @@ export default function Home() {
       <main>
         <Layout>
           <Shop></Shop>
+          <Link href='/cart'>
+            <BsCart4 className='text-5xl rounded bg-yellow-500 fixed bottom-16 right-10 cursor-pointer' />
+          </Link>
         </Layout>
       </main>
     </>
