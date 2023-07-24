c0b7ec9adcd6e4a6dac1bb60dc0d2c719712c1d6
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-07-25 01:51:57 +0600

    showing admin and dman role in the ui

--- src/components/Dashboard/ManageUsers/User.js
+++ src/components/Dashboard/ManageUsers/User.js
@@ -44,7 +44,10 @@ const User = ({ user, refetch }) => {
                     </>
                 }
 
-
+                {
+                    role !== "user" &&
+                    <p className='text-center mx-12 rounded-lg uppercase font-semibold text-xs mt-1.5 bg-blue-300'>{role}</p>
+                }
 
             </div>
 
