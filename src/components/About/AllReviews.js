6b506a9e30bfbd4cc678e45acc2116882c679d3c
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-08-01 14:08:17 +0600

    all the reviews showing in the UI

--- src/components/About/AllReviews.js
+++ src/components/About/AllReviews.js
@@ -19,7 +19,32 @@ const AllReviews = () => {
         <section>
             <h2 className='text-orange-500 font-serif text-center text-3xl font-bold mt-12 mb-4'>Customer&rsquo;s Say About Us</h2>
 
-            <p className='text-red-500 italic text-center text-2xl font-bold mt-12'>No Review Yet</p>
+            <div className='overflow-x-auto mx-36 h-96'>
+                {
+                    reviews?.length === 0
+                        ?
+                        <p className='text-red-500 italic text-center text-2xl font-bold mt-12'>No Review Yet</p>
+                        :
+                        reviews.map((review, index) => <>
+
+                            <div className={`chat ${index % 2 !== 0 ? 'chat-start' : 'chat-end'} `}>
+                                <div className="chat-header mb-1">
+                                    {review.name}
+                                    <span className="text-xs opacity-75 ml-1.5">{review.email}</span>
+                                </div>
+
+                                <div className="chat-bubble italic">
+                                    {review?.feedback}
+                                    <br />
+                                    Ratings {review?.ratings}
+                                </div>
+
+                            </div>
+                        </>)
+                }
+            </div>
+
+
         </section>
     );
 };
