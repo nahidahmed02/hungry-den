b89ea0c666cbb18d258a1780f7ea6680bf8e9bcd
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-08-02 03:43:07 +0600

    user can delete the reviews given by self

--- src/components/Dashboard/AddReview/MyReview.js
+++ src/components/Dashboard/AddReview/MyReview.js
@@ -2,12 +2,13 @@ import React, { useContext } from 'react';
 import Loading from '../../Loading/Loading';
 import { useQuery } from 'react-query';
 import { AuthContext } from '@/src/context/AuthProvider';
+import { toast } from 'react-hot-toast';
 
 const MyReview = () => {
     const { user } = useContext(AuthContext);
     const email = user.email;
 
-    const { data: myReviews, isLoading } = useQuery({
+    const { data: myReviews, isLoading, refetch } = useQuery({
         queryKey: ['reviews'],
         queryFn: async () => {
             const res = await fetch(`http://localhost:5000/reviews/${email}`);
@@ -20,6 +21,20 @@ const MyReview = () => {
         return <Loading></Loading>
     }
 
+    const handleDeleteReview = id => {
+        fetch(`http://localhost:5000/reviews/${id}`, {
+            method: 'DELETE',
+
+        })
+            .then(res => res.json())
+            .then(data => {
+                if (data.deletedCount) {
+                    toast.success('Review Deleted')
+                    refetch();
+                }
+            })
+    }
+
     return (
         <section>
             <h2 className='lg:mt-16 mb-4 text-2xl font-serif font-bold text-orange-500 text-center'>My Reviews</h2>
@@ -30,7 +45,20 @@ const MyReview = () => {
                         ?
                         <p className='text-red-500 italic text-center text-2xl font-bold mt-12'>No Review Yet</p>
                         :
-                        myReviews?.map(review => <p key={review._id}>{review.feedback}</p>)
+                        myReviews?.map(review =>
+                            <div key={review._id} className='border border-yellow-500 rounded mx-32 mb-2'>
+
+                                <p className='italic font-semibold'>{review.feedback}</p>
+                                <p>Ratings Given: {review.ratings}</p>
+
+                                <button
+                                    onClick={() => handleDeleteReview(review?._id)}
+                                    className='bg-red-500 px-2 rounded-lg text-sm mb-2 text-white'
+                                >
+                                    Delete
+                                </button>
+
+                            </div>)
                 }
             </div>
         </section>
