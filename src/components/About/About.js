a51a0bac2cd05781341a6e6a26ff8e711df97f07
Nahid Ahmed <ahmednahid1995@gmail.com>
2023-08-18 23:57:26 +0600

    updated UI of about us

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,15 +6,15 @@ const About = () => {
         <section className='pt-24 mb-16'>
             <h2 className='text-orange-500 font-serif text-center text-3xl font-bold mb-4'>Who We Are</h2>
 
-            <p className='mx-28'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod quae sunt mollitia eius ducimus minus? Exercitationem, dolore? Et aliquam ut quibusdam, quia dignissimos impedit quo error reprehenderit sunt natus alias labore id perferendis reiciendis doloremque ratione eveniet explicabo hic dicta, odit facere suscipit eius excepturi quaerat. Minus tenetur possimus laudantium!</p>
-
-            <p className='font-semibold mx-28 mt-5'>Open: Mon-Fri (9am - 9pm)</p>
-            <p className='font-semibold mx-28 mt-2'>Closing: Sat & Sun</p>
-            <p className='font-semibold mx-28 mt-2'>Location: Poland</p>
-            <p className='font-semibold mx-28 mt-2'>Contact Us:</p>
-            <p className='font-semibold mx-40 mt-2'>Phone: +452311123</p>
-            <p className='font-semibold mx-40 mt-2'>Facebook: fb.afef.com</p>
-            <p className='font-semibold mx-40 mt-2'>Mail: www.aeryj@gmail.com</p>
+            <p className='text-white mx-28'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quod quae sunt mollitia eius ducimus minus? Exercitationem, dolore? Et aliquam ut quibusdam, quia dignissimos impedit quo error reprehenderit sunt natus alias labore id perferendis reiciendis doloremque ratione eveniet explicabo hic dicta, odit facere suscipit eius excepturi quaerat. Minus tenetur possimus laudantium!</p>
+            text-white
+            <p className='text-white font-semibold mx-28 mt-5'>Open: Mon-Fri (9am - 9pm)</p>
+            <p className='text-white font-semibold mx-28 mt-2'>Closing: Sat & Sun</p>
+            <p className='text-white font-semibold mx-28 mt-2'>Location: Poland</p>
+            <p className='text-white font-semibold mx-28 mt-2'>Contact Us:</p>
+            <p className='text-white font-semibold mx-40 mt-2'>Phone: +452311123</p>
+            <p className='text-white font-semibold mx-40 mt-2'>Facebook: fb.afef.com</p>
+            <p className='text-white font-semibold mx-40 mt-2'>Mail: www.aeryj@gmail.com</p>
 
             <AllReviews></AllReviews>
 
